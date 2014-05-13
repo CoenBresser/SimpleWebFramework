f9e1658bdef3e50a4f524dbf479b684b8d09ff98
CoenBresser <coenbresser@gmail.com>
2014-05-13 15:36:20 +0200

    Finished version 2 start
    CRUD functionality complete, filtering is possible by url parameter

--- server/app/data/Interfaces/v2.0.php
+++ server/app/data/Interfaces/v2.0.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * v1.2 interfaces 
+ * v2.0 interfaces 
  * 
  * This version uses a better way of indexing the files on the server in a tree like manner: 
  * Section 
@@ -19,6 +19,21 @@ class v2_0_Data {
     $this->fileName = $this::file_base . urlencode($file);
   }
   
+  public static function dbExists($name) {
+    $filename = v2_0_Data::file_base . urlencode($name);
+    return file_exists($filename);
+  }
+  
+  public static function deleteDb($name) {
+    $filename = v2_0_Data::file_base . urlencode($name);
+    $success = unlink($filename);
+    // try without checking result
+    if (file_exists($filename.'.lck')) {
+      $success = $success && unlink($filename.'.lck');
+    }
+    return $success;
+  }
+  
   public function exists($key) {
     $dbh = dba_open($this->fileName, 'r');
     $value = dba_exists($key, $dbh);
@@ -31,11 +46,20 @@ class v2_0_Data {
    *
    * The database is closed when the function exits
    */
-  public function getValue($key) {
+  public function getValue($key, $hidePassword = true) {
+    $isUsersFile = ($this->fileName === v2_0_Data::file_base . urlencode('users'));
     $dbh = dba_open($this->fileName, 'r');
-    $value = dba_fetch($key, $dbh);
+    $val = dba_fetch($key, $dbh);
+
+    // Don't send passwords
+    if ($hidePassword && $isUsersFile) {
+      $eval = json_decode($val, true);
+      unset($eval['password']);
+      $val = json_encode($eval);
+    }
+
     dba_close($dbh);
-    return $value;
+    return $val;
   }
 
   /**
@@ -43,14 +67,65 @@ class v2_0_Data {
    *
    * The database is closed when the function exits
    */
-  public function getValues() {
+  public function getValues($filter = null, $hidePassword = true) {
     $dbh = dba_open($this->fileName, 'rl');
+    $isUsersFile = ($this->fileName === v2_0_Data::file_base . urlencode('users'));
     $jsonArray = '[';
     if ($the_key = dba_firstkey($dbh)) do {
-       $jsonArray = $jsonArray . dba_fetch($the_key, $dbh) . ',';
+      $val = dba_fetch($the_key, $dbh);
+
+      // Don't send passwords
+      if ($hidePassword && $isUsersFile) {
+        $eval = json_decode($val, true);
+        unset($eval['password']);
+        $val = json_encode($eval);
+      }
+      
+      // Check if we should filter. This could be optimized by splitting the loop in a filtered and non filtered loop
+      // Not too big databases so skip above optimization
+      if ($filter) {
+        $eval = json_decode($val, true);
+        foreach ($filter as $key => $value) {
+          if (array_key_exists($key, $eval)) {
+            $availableVal = $eval[$key];
+            if (is_array($availableVal)) {
+              if (in_array($value, $availableVal)) {
+                $jsonArray = $jsonArray . $val . ',';
+                break;
+              }
+            } else if ($availableVal == $value) {
+              $jsonArray = $jsonArray . $val . ',';
+              break;
+            }
+          }
+        }
+      } else {
+        $jsonArray = $jsonArray . $val . ',';
+      }
     } while ($the_key = dba_nextkey($dbh));
     dba_close($dbh);
     return rtrim($jsonArray, ",") . ']';
+  }
+  
+  public function write($key, $value) {
+    $dbh = dba_open($this->fileName, 'wl');
+    $success = dba_insert($key, $value, $dbh);
+    dba_close($dbh);
+    return $success;
+  }
+  
+  public function replace($key, $value) {
+    $dbh = dba_open($this->fileName, 'wl');
+    $success = dba_replace($key, $value, $dbh);
+    dba_close($dbh);
+    return $success;
+  }
+  
+  public function delete($key) {
+    $dbh = dba_open($this->fileName, 'wl');
+    $success = dba_delete($key, $dbh);
+    dba_close($dbh);
+    return $success;
   }
   
   public static function checkCreateDb($name, $initialContentArray = '') {
@@ -92,7 +167,7 @@ class CredentialDb implements iUserCredentials {
   protected $udb;
   
   public function __construct() {
-    $this->udb = v2_0_Data::checkCreateDb('users', array('admin' => '{"password":"joke2704", "role": "' . iUserCredentials::ADMIN . '"}'));
+    $this->udb = v2_0_Data::checkCreateDb('users', array('admin' => '{"id":"admin", "password":"joke2704", "role": "' . iUserCredentials::ADMIN . '"}'));
   }
   
   public function getUserRole($username) {
@@ -101,9 +176,10 @@ class CredentialDb implements iUserCredentials {
   }
   
   public function getUserPassword($username) {
-    $uInfo = json_decode($this->udb->getValue($username), true); 
+    $uInfo = json_decode($this->udb->getValue($username, false), true); 
     return $uInfo["password"];
   }
+  
   public function userExists($username) {
     return $this->udb->exists($username);
   }
@@ -124,25 +200,44 @@ class v2_0_Interface {
     
     $helper = $this;
     $app->group('/v2.0', function () use ($app, $helper) {
-
+      // Admin
       $app->get('/', function () use ($app, $helper) { $helper->doAdmin($app); });
       
       // Data paths
-      $app->get('/:file(/(:id))', function ($file, $id=false) use ($app, $helper) { $helper->checkAuth($file, $id, $app, iUserCredentials::USER); $helper->getFile($file, $id, $app); });
-      $app->put('/:file(/(:id))', function ($file, $id=false) use ($app, $helper) { $helper->checkAuth($file, $id, $app); $helper->putFile($file, $id, $app); });
-      $app->patch('/:file(/(:id))', function ($file, $id=false) use ($app, $helper) { $helper->checkAuth($file, $id, $app); $helper->patchFile($file, $id, $app); });
-      $app->delete('/:file(/(:id))', function ($file, $id=false) use ($app, $helper) { $helper->checkAuth($file, $id, $app); $helper->deleteFile($file, $id, $app); });
+      $app->get('/:file(/(:id))', function ($file, $id=false) use ($app, $helper) { $helper->getFile($file, $id, $app); });
+      $app->put('/:file(/(:id))', function ($file, $id=false) use ($app, $helper) { $helper->putFile($file, $id, $app); });
+      $app->patch('/:file(/(:id))', function ($file, $id=false) use ($app, $helper) { $helper->patchFile($file, $id, $app); });
+      $app->delete('/:file(/(:id))', function ($file, $id=false) use ($app, $helper) { $helper->deleteFile($file, $id, $app); });
     });
+  }
+
+  protected function requestHasVar($varName, $app) {
+    return array_key_exists($varName, $app->request->get());
   }
 
   public function doAdmin($app) {
     $this->auth->assertUserRoleIsAtLeast(iUserCredentials::ADMIN);
-    // TODO: list db's, add db's, delete db's
-    // TODO: edit db's: list entries, add entries, edit entries, delete entries
+    
+    // Check for special requests
+    if ($this->requestHasVar('dbList', $app)) {
+      $files = scandir(v2_0_Data::file_base);
+      $resp = "[";
+      foreach ($files as $file) {
+        if ($file === ".") { continue; }
+        if ($file === "..") { continue; }
+        if (Util::endsWith($file, ".lck")) { continue; }
+        if (is_dir($file)) { continue; }
+        $resp = $resp . '{"name":"' . $file . '"},';
+      }
+      echo rtrim($resp, ",") . ']';
+    } else {
+      // return the datafrontend application
+      include 'v2.0_frontend.html';
+    }
   }
   
   // check authorization (must be public due to helper...)
-  public function checkAuth($file, $id, $app, $itemRole = iUserCredentials::MEMBER, $listRole = iUserCredentials::SITE_ADMIN) {
+  public function checkAuth($file, $id, $app, $itemRole = iUserCredentials::MEMBER, $listRole = c) {
     // Checking existence could be done here, skip it as it could be used by 'someone' to see what exists or not
     if ($id) {
       $this->auth->assertUserRoleIsAtLeast($itemRole);
@@ -158,10 +253,15 @@ class v2_0_Interface {
   }
   
   public function getFile($file, $id, $app) {
+    $params = $app->request->params();
+    $listAuth = $params ? iUserCredentials::USER : $listRole = iUserCredentials::SITE_ADMIN;
+    $this->checkAuth($file, $id, $app, iUserCredentials::USER, $listAuth);
+    
     try {
       $db = new v2_0_Data($file);
       if (!$id) {
-        $app->response->body($db->getValues());
+        $params = $app->request->params();
+        $app->response->body($db->getValues($params));
       } else {
         $app->response->body($db->getValue($id));
       }
@@ -171,26 +271,110 @@ class v2_0_Interface {
   }
 
   public function putFile($file, $id, $app) {
+    $this->checkAuth($file, $id, $app, iUserCredentials::MEMBER, iUserCredentials::ADMIN); 
+    
     if (!$id) {
-      // series of put
+      // creation of db
+      if (!v2_0_Data::dbExists($file)) {
+        v2_0_Data::checkCreateDb($file);
+        $app->response->setStatus(201); // created
+        $app->response->write($app->request->getBody());
+      } else {
+        $app->response->setStatus(409); // conflict
+      }
+      
+      // check for body, if available then initial load
+      $initContent = json_decode($app->request->getBody(), true);
+      // TODO!!!
     } else {
-      // individual put
+      // individual put, check key existence
+      if (!v2_0_Data::dbExists($file)) {
+        $app->notFound();
+      } else {
+        $db = new v2_0_Data($file);
+        if ($db->exists($id)) {
+          $app->response->setStatus(409); // conflict
+        } else {
+          $content = $app->request->getBody();
+          if ($content) {
+            if ($db->write($id, $content)) {
+              $app->response->setStatus(201); // created
+              $app->response->write($content);
+            } else {
+              // unknown error
+              $app->error();
+            }
+          } else {
+            $app->response->setStatus(400); // bad request
+          }
+        }
+      }
     }
   }
 
   public function patchFile($file, $id, $app) {
+    $this->checkAuth($file, $id, $app, iUserCredentials::MEMBER, iUserCredentials::SITE_ADMIN); 
+    
     if (!$id) {
-      // series of put
+      // series of patch
+      $app->response->setStatus(405); // not implemented yet, method not allowed
     } else {
-      // individual put
+      // individual patch, check key existence
+      if (!v2_0_Data::dbExists($file)) {
+        $app->notFound();
+      } else {
+        $db = new v2_0_Data($file);
+        if (!$db->exists($id)) {
+          $app->response->setStatus(409); // try to patch non existent object
+        } else {
+          $content = $app->request->getBody();
+          if ($content) {
+            if ($db->replace($id, $content)) {
+              $app->response->setStatus(202); // accepted
+              $app->response->write($content);
+            } else {
+              // unknown error
+              $app->error();
+            }
+          } else {
+            $app->response->setStatus(400); // bad request
+          }
+        }
+      }
     }
   }
 
   public function deleteFile($file, $id, $app) {
+    $this->checkAuth($file, $id, $app, iUserCredentials::MEMBER, iUserCredentials::ADMIN); 
+    
     if (!$id) {
-      // check for admin, series of put
+      
+      if (v2_0_Data::dbExists($file)) {
+        if (v2_0_Data::deleteDb($file)) {
+          $app->response->setStatus(204); // no content
+        } else {
+          // unknown error
+          $app->error();
+        }
+      } else {
+        $app->notFound(); // db not found
+      }
     } else {
-      // check for user, individual put
+      if (!v2_0_Data::dbExists($file)) {
+        $app->notFound();
+      } else {
+        $db = new v2_0_Data($file);
+        if (!$db->exists($id)) {
+          $app->notFound(); // nothing to delete
+        } else {
+          if ($db->delete($id, $content)) {
+            $app->response->setStatus(204); // no content
+          } else {
+            // unknown error
+            $app->error();
+          }
+        }
+      }
     }
   }
 }
@@ -350,16 +534,19 @@ class HttpDigestAuth {
     }
     return $needed_parts ? false : $data;
   }
+}
+
+class Util {
   
-  protected function contains($haystack, $needle) {
+  static function contains($haystack, $needle) {
     return strpos($haystack,$needle) !== false;
   }
   
-  protected function startsWith($haystack, $needle) {
+  static function startsWith($haystack, $needle) {
     return $needle === "" || strpos($haystack, $needle) === 0;
   }
   
-  protected function endsWith($haystack, $needle) {
+  static function endsWith($haystack, $needle) {
     return $needle === "" || substr($haystack, -strlen($needle)) === $needle;
   }
 }
